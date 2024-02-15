a60541b5dbc119431ac1f8088a947180e476872a
Edgar Manuel Pérez Cob <59448174+edgarp9504@users.noreply.github.com>
2024-02-15 11:11:22 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,9 @@ import streamlit
 streamlit.title('My parents New Healthy diner')
 
 streamlit.header('Breakfast Menu')
-streamlit.text('My parents New Healthy diner')
-streamlit.text('My parents New Healthy diner')
-streamlit.text('My parents New Healthy diner')
+streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔Hard-Boiled Free-Range Egg')
+
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
