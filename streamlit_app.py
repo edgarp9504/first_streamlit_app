3cd0ccbee46e65e9508e590a2d61dc00f1d089ce
Edgar Manuel Pérez Cob <59448174+edgarp9504@users.noreply.github.com>
2024-02-15 11:10:01 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
-
 import streamlit
+
 streamlit.title('My parents New Healthy diner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('My parents New Healthy diner')
+streamlit.text('My parents New Healthy diner')
+streamlit.text('My parents New Healthy diner')
